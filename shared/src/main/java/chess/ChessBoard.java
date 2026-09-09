00c6d18be7fea89b96e78dd851262369c22b3756
Gavin Mobley <gavinwm@byu.edu>
2026-09-09 10:35:46 -0600

    in class work filling in methods

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+private ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -21,7 +21,7 @@ public class ChessBoard {
     public void addPiece(ChessPosition position, ChessPiece piece) {
         throw new RuntimeException("Not implemented");
     }
-
+        board[position.getRow()][position.getColumn()] = piece;
     /**
      * Gets a chess piece on the chessboard
      *
@@ -32,7 +32,7 @@ public class ChessBoard {
     public ChessPiece getPiece(ChessPosition position) {
         throw new RuntimeException("Not implemented");
     }
-
+        return board[position.getRow() - 1][position.getColumn() - 1];
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
